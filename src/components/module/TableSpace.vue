35904e9ab0327d5742348cb5c5872af6b79cd901
roy <neo.work@foxmail.com>
2021-01-14 00:39:26 +0800

    初始化组件

---
+++ src/components/module/TableSpace.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>
+      <UserTableSpace/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {
+    UserTableSpace: resolve => {
+      require(['@/components/module/table/UserTableSpace'], resolve)
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Element from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+import VueCookies from 'vue-cookies'
+import echarts from 'echarts'
+import components from './plugins/Component' //加载公共组件
 
+Vue.prototype.$echarts = echarts
+Vue.use(VueCookies)
+Vue.use(Element)
 Vue.config.productionTip = false
+Object.keys(components).forEach((key) => {
+    var name = key.replace(/(\w)/, (v) => v.toUpperCase())
+    Vue.component('v' + name, components[key])
+})
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app')
